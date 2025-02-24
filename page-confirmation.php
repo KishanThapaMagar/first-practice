2a67a389718d6b3f4a2f1ed71b50e83585d210bc
private ktm <aiden-nika>
2025-02-24 13:08:35 +0545

    hello third'

--- page-confirmation.php
+++ page-confirmation.php
@@ -7,7 +7,7 @@ get_header();
 <div class="app-con">
     <h2>Booking Confirmed!</h2>
     <p>Thank you! Your appointment has been scheduled. We'll contact you soon.</p>
-    <a href="<?php echo site_url('/booking'); ?>">Back to Home</a>
+    <a href="<?php echo site_url('/booking'); ?>">Back</a>
 </div>
 
 <?php get_footer(); ?>
